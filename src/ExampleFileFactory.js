483380a5e4aa9a7d6c0636944011f6580705a255
Anders Jurisoo <jurisoo@hotmail.com>
2019-07-24 12:13:48 +0200

    Cleaned up

--- src/ExampleFileFactory.js
+++ src/ExampleFileFactory.js
@@ -1,11 +1,46 @@
-import { BaseFileFactory } from 'core'
-
- export default class ExampleFileFactory extends BaseFileFactory {
+ export default class ExampleFileFactory {
     constructor() {
         this.pipes = []
     }
 
-     static get name() {
-        return "Just an Example"
+    static get title() {
+        return "ExampleFileFactory"
+    }
+
+    static templates() {
+        return {}
+    }
+
+    static version() {
+        return require('../package.json').version;
+    }
+
+    static settings() {
+        return [
+            {
+                name: "ExampleFileFactory dummy setting",
+                default: "Anything here...",
+                dataType: String,
+            }            
+        ]
+    }
+
+    static pipes() {
+        return [
+            
+        ]
+    }
+
+    static defaultPreferences() {
+        return {}
+    }
+
+    static from(objectModelCollection) {
+        return new this(objectModelCollection)
+    }
+
+    withPipes(pipes) {
+        this.pipes = pipes
+        return this
     }
 } 
